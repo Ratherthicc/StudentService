eab44974cc29ef2a0a1980ff1db29ab4ac96af42
Nedeljko Babic <bnedeljko0@gmail.com>
2022-12-28 23:11:13 +0100

    dodati kontroleri za studenta i profesora

---
+++ Studentska Sluzba/StudentskaWPF/Controller/ProfesorController.cs
@@ -0,0 +1,48 @@
+﻿using StudentskaSluzba.model;
+using StudentskaWPF.DAOModel;
+using StudentskaWPF.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentskaWPF.Controller
+{
+    class ProfesorController
+    {
+
+        private DAOProfesor profesori;
+
+        public ProfesorController()
+        {
+            profesori = new DAOProfesor();
+        }
+
+        public List<Profesor> GetAllProfesor()
+        {
+            return profesori.GetAll();
+        }
+
+        public void Create(Profesor profesor)
+        {
+            profesori.Add(profesor);
+        }
+
+        public void Delete(Profesor profesor)
+        {
+            profesori.Remove(profesor);
+        }
+
+        public void Update(Profesor profesor)
+        {
+         //   profesori.Update(profesor);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            profesori.Subscribe(observer);
+        }
+    
+}
+}
---
+++ Studentska Sluzba/StudentskaWPF/Controller/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using StudentskaSluzba.model;
+using StudentskaWPF.DAOModel;
+using StudentskaWPF.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentskaWPF.Controller
+{
+    class StudentController
+    {
+
+        private DAOStudent studenti;
+
+        public StudentController()
+        {
+            studenti = new DAOStudent();
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return studenti.GetAll();
+        }
+
+        public void Create(Student student)
+        {
+            studenti.Add(student);
+        }
+
+        public void Delete(Student student)
+        {
+            studenti.Remove(student);
+        }
+
+        public void Update(Student student)
+        {
+          //  studenti.Update(student);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            studenti.Subscribe(observer);
+        }
+    }
+}
+
